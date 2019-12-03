f82f5db34ba9e265a8d45722fe74fc35f5c1a056
sacomy <sacomy.sacomy@gmail.com>
2019-12-03 12:13:12 +0900

    フォルダの更新周りを修正した

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 if [ ! -e ~/.vimrc_default ]; then
   cp ~/.vimrc ~/.vimrc_default
 fi
+if [ -e ~/mydotfiles ]; then
+  rm -rf ~/.dotfiles
+  mv ~/mydotfiles ~/.dotfiles
+fi
 cp ~/.dotfiles/.vimrc ~/.vimrc
 source ~/.bashrc
 echo "finish!"
-# rm -rf ~/.dotfiles
